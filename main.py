7803bb5587d49e810215f26289bfe83210cbfa0f
bananaFish001 <raj123.rs308@gmail.com>
2024-04-11 21:39:46 +0530

    retreiving data using get method

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-async def root():
+async def root(): # async is only used when doing tasks that may take time
     return {"message": "Welcome to NHK"}
+
+@app.get("/posts")
+async def get_post():
+    return {"data": "This is your post."}
