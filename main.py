7e5ddbaa65dd5acaa054d5d264176998d539aaa9
bananaFish001 <raj123.rs308@gmail.com>
2024-04-11 22:00:00 +0530

    creating and viewing the posts

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.params import Body
 app = FastAPI()
 
 @app.get("/")
@@ -9,3 +9,8 @@ async def root(): # async is only used when doing tasks that may take time
 @app.get("/posts")
 async def get_post():
     return {"data": "This is your post."}
+
+@app.post("/createposts")
+async def post_data(payload: dict = Body(...)):
+    print(payload)
+    return {"message": "successfully created a post"}
